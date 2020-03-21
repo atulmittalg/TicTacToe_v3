ad35ea0791c57a8f8c941db1445a5470cfb404fb
Atul Mittal <55511902+atulmittalg@users.noreply.github.com>
2020-03-21 10:11:06 +0100

    Static import of Assert statements and Using class level TicTacToeGame Object.

--- src/test/java/com/kata/tictactoe/TicTacToeGameTest.java
+++ src/test/java/com/kata/tictactoe/TicTacToeGameTest.java
@@ -1,19 +1,28 @@
 package com.kata.tictactoe;
 
-import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 public class TicTacToeGameTest {
+
+    private TicTacToeGame ticTacToeGame;
+
+    @Before
+    public void initializeTicTacToeGameObject(){
+        ticTacToeGame = new TicTacToeGame();
+    }
+
     @Test
     public void shouldBeAbleToCreateTicTacToeObject(){
-        TicTacToeGame ticTacToeGame = new TicTacToeGame();
-        Assert.assertNotNull(ticTacToeGame);
+        assertNotNull(ticTacToeGame);
     }
 
     @Test
     public void shouldAssignTokenXWhenFirstTurnIsPlayed(){
-        TicTacToeGame ticTacToeGame = new TicTacToeGame();
         ticTacToeGame.playTurnAt(0,0);
-        Assert.assertEquals('X', ticTacToeGame.getTokenAt(0,0));
+        assertEquals('X', ticTacToeGame.getTokenAt(0,0));
     }
 }
