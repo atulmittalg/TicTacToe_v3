cd61ef9f44472355cf70f7d6b6428199c00e1cd0
Atul Mittal <55511902+atulmittalg@users.noreply.github.com>
2020-03-22 00:00:19 +0100

    Refactor: Using constant for Empty position, and removing token 'O' constant as used only once.

--- src/main/java/com/kata/tictactoe/TicTacToeGame.java
+++ src/main/java/com/kata/tictactoe/TicTacToeGame.java
@@ -2,12 +2,12 @@ package com.kata.tictactoe;
 
 public class TicTacToeGame {
     private final char TOKEN_X = 'X';
-    private final char TOKEN_O = 'O';
+    private final char EMPTY_POSITION = '\0';
     private char[][] gameBoardLayout = new char[3][3];
     private char currentTokenToPlay = TOKEN_X;
 
     public String playTurnAt(final int positionX, final int positionY) throws PositionAlreadyInUseException {
-        if(gameBoardLayout[positionX][positionY] != '\0'){
+        if(gameBoardLayout[positionX][positionY] != EMPTY_POSITION){
             throw new PositionAlreadyInUseException(positionX, positionY);
         }
         gameBoardLayout[positionX][positionY] = currentTokenToPlay;
@@ -28,20 +28,20 @@ public class TicTacToeGame {
     }
 
     private boolean isWinnerByRightToLeftDiagonal() {
-        return (getTokenAt(0, 2) != '\0') &&
+        return (getTokenAt(0, 2) != EMPTY_POSITION) &&
                 (getTokenAt(0, 2) == getTokenAt(1, 1)) &&
                 (getTokenAt(0, 2) == (getTokenAt(2, 0)));
     }
 
     private boolean isWinnerByLeftToRightDiagonal() {
-        return (getTokenAt(0, 0) != '\0') &&
+        return (getTokenAt(0, 0) != EMPTY_POSITION) &&
                 (getTokenAt(0, 0) == getTokenAt(1, 1)) &&
                 (getTokenAt(0, 0) == (getTokenAt(2, 2)));
     }
 
     private boolean isWinnerByColumn(){
         for(int column = 0; column <3 ; column++){
-            if ((getTokenAt(0, column) != '\0') &&
+            if ((getTokenAt(0, column) != EMPTY_POSITION) &&
                     (getTokenAt(0, column) == getTokenAt(1, column)) &&
                     (getTokenAt(0, column) == getTokenAt(2, column))) {
                 return true;
@@ -52,7 +52,7 @@ public class TicTacToeGame {
 
     private boolean isWinnerByRow() {
         for(int row = 0; row <3 ; row++){
-            if ((getTokenAt(row,0) != '\0') &&
+            if ((getTokenAt(row,0) != EMPTY_POSITION) &&
                     (getTokenAt(row,0) == getTokenAt(row,1)) &&
                     (getTokenAt(row,0) == (getTokenAt(row,2)))) {
                 return true;
@@ -62,7 +62,7 @@ public class TicTacToeGame {
     }
 
     private void setCurrentTokenToPlayValueForNextTurn() {
-        currentTokenToPlay = (currentTokenToPlay == TOKEN_X) ? TOKEN_O : TOKEN_X;
+        currentTokenToPlay = (currentTokenToPlay == TOKEN_X) ? 'O' : TOKEN_X;
     }
 
     public char getTokenAt(final int positionX, final int positionY) {
